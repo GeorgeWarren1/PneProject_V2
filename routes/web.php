c4357455fcb49ad22fa473e9f2d8401776c76306
GeorgeWarren1 <george.warren.pne@gmail.com>
2024-06-27 11:18:08 -0400

    change env

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\dbController;
 
 Route::get('/managers/position/{value}/view', [dbController::class, 'getManagersByPosition']);
 Route::get('/managers/view', [dbController::class, 'getManagersByPosition']);
-
+    
 Route::get('/', function () {
     return view('welcome');
 });
